226f0eed005802728fd9cf02783ebe28e321d2f7
Phongfor <hoangphong2725@gmail.com>
2026-05-14 10:05:22 +0700

    feat: add Redis Cache for Product

--- src/main/java/com/novafurniture/NovaFurniture/dto/response/PageResponse.java
+++ src/main/java/com/novafurniture/NovaFurniture/dto/response/PageResponse.java
@@ -2,13 +2,14 @@ package com.novafurniture.NovaFurniture.dto.response;
 
 import lombok.*;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class PageResponse<T> {
+public class PageResponse<T> implements Serializable {
     private List<T> content;
     private int pageNumber;
     private int pageSize;
