3b1ee80665afcf8a3a04b0d7e1b81c799fd4a9fb
Phongfor <hoangphong2725@gmail.com>
2026-05-16 17:22:37 +0700

    refactor: move project into be/ subdirectory

---
+++ fe/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+    return (
+        <>
+            <div className='bg-black min-h-screen text-white flex items-center justify-center'>
+                <h1 className='text-6xl font-light tracking-widest'>
+                    NOVA FURNITURE
+                </h1>
+            </div>
+        </>
+    );
+}
+export default App;
