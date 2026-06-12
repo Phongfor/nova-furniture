48a530cd9dcc633e1d6cae5f0c0a37ce6e0770e1
Phongfor <hoangphong2725@gmail.com>
2026-06-12 09:44:04 +0700

    fix: correct axiosClient import path in OAuth2Callback

--- fe/src/pages/client/OAuth2Callback/OAuth2Callback.jsx
+++ fe/src/pages/client/OAuth2Callback/OAuth2Callback.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import axiosClient from '../api/axiosClient'
+import axiosClient from '../../../services/axiosClient'
+
 
 const OAuth2Callback = () => {
   const navigate = useNavigate()
